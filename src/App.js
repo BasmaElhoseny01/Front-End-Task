5b2aead24571257a925fff89df5fcec2faf27250
BasmaElhoseny <basmaelhoseny6@gmail.com>
2022-09-30 21:47:15 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 //import Router
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 //styling
 import './App.css';
 
 import Sigin from './components/Sigin';
 import Signup from './components/Signup';
-
 import Social from './components/Social';
-
 import Forget from './components/Forget';
 
 function App() {
+    useEffect(() => {
+      document.title = "Cairo University Eco Team"
+    }, [])
+
   return (
     <div className="App">
         <Router>
         <h1>Welcome</h1>
         <Routes>
     
-          <Route path="/" element={<Sigin/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/social" element={<Social/>}/>
+          <Route path="/Front-End-Task" element={<Sigin/>}/>
+          <Route path="/Front-End-Task/signup" element={<Signup/>}/>
+          <Route path="/Front-End-Task/social" element={<Social/>}/>
 
-          <Route path="/forgetpassword" element={<Forget/>}/>
+          <Route path="/Front-End-Task/forgetpassword" element={<Forget/>}/>
           
 
         </Routes>
